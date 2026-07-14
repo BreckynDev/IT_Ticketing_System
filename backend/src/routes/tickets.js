32608d4233059457eb6dc1384dadaee80debdafd
BIBS <breckynburke@gmail.com>
2026-07-14 12:45:50 -0600

    Added GET endpoints
    In ticket router, added two GET endpoints to retrieve data. First fetches all tickets, the other fetches a single ticket by ID.

--- backend/src/routes/tickets.js
+++ backend/src/routes/tickets.js
@@ -3,6 +3,7 @@ import { sql } from "../db.js"
 
 const router = Router();
 
+// Create Ticket
 router.post('/', async (req, res) => {
   const { name, email, department, category, description, device_info } = req.body;
 
@@ -25,8 +26,47 @@ router.post('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
-    res.send("Ticket retrieved")
+// Get all Tickets
+router.get('/', async (req, res) => {
+
+        const tickets = await sql `
+        SELECT * FROM tickets`
+
+        res.status(200).json({ success: true, data: tickets})
+
+    } catch (error) {
+        console.log("Error in retrieving all tickets")
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+})
+
+// Get one Ticket
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const ticket = await sql `
+        SELECT * FROM tickets WHERE id=${id}`
+
+        if (ticket.length === 0) {
+            return res.status(404).json({ success: false, message: "Ticket not found" });
+        }
+
+        res.status(200).json({ success: true, data: ticket[0]})
+
+    } catch (error) {
+        console.log("Error in retrieving ticket ", error)
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+})
+
+// Update a ticket
+router.patch('/:id', (req, res) => {
+    res.send("Ticket Updated")
+})
+
+// remove a ticket
+router.delete('/:id', (req, res) => {
+    res.send("Ticket Remover")
 })
 
 export default router;
